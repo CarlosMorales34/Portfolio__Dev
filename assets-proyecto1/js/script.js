e322ab2f3e67de077508a00fdf59950e999ca52d
Carlos Morales <carlos.mrojas@alumnos.udg.mx>
2023-10-31 13:22:26 -0600

    toggle Menu responsive

--- assets-proyecto1/js/script.js
+++ assets-proyecto1/js/script.js
@@ -1,3 +1,4 @@
+// MAPA
 document.addEventListener("DOMContentLoaded", (event) => {
     setTimeout( () => {
         document.querySelector("#load-iframe-map").innerHTML = `
@@ -6,4 +7,35 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }, 3000);
     
 });
+// TOGGLE
+document.addEventListener("DOMContentLoaded", (event) => {
+    let button = document.querySelector(".layout__menu-toggle");
+    let icon_bars = document.querySelector(".layout__menu-toggle .fa-bars");
+    let icon_mark = document.querySelector(".layout__menu-toggle .fa-xmark");
+    let aside = document.querySelector(".layout__aside");
+    
+    button.addEventListener("click", (event) => {
+        let visible = document.querySelector(".layout__aside--visible");
+        if (!visible){
+            aside.classList.add("layout__aside--visible");
+            icon_bars.style.opacity = 0;
+            icon_mark.style.opacity = 1;
+        } else{
+            aside.classList.remove("layout__aside--visible");
+            icon_bars.style.opacity = 1;
+            icon_mark.style.opacity = 0;
+        }
+        });
+         // RESIZE
+    window.addEventListener("resize", () => {
+        let size = parseInt(document.body.clientWidth);
+        if(size <= 1060){
+            aside.classList.remove("layout__aside--visible");
+            icon_bars.style.opacity = 1;
+            icon_mark.style.opacity = 0;
+        }
+    });
+});
+
+
 
