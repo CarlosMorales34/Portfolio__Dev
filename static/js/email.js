f43a8160a51a1500cfad2bd46d04209bf1eae3c7
CarlosMorales34 <carlos.mrojas@alumnos.udg.mx>
2024-08-26 17:09:00 -0600

    Sin resolver el email pero con forma de contacto whatsapp

--- static/js/email.js
+++ static/js/email.js
@@ -1,26 +1,18 @@
-import { Resend } from 'resend';
-
-const resend = new Resend('re_7DViJ2pv_7ZywKjk7uHEBQCHQexXXr53f');
-
-document.querySelector('.form__button').addEventListener('click', () => {
-    console.log("btn clickeado");
-    // Obtener los valores del formulario
-    const nombre = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const asunto = document.getElementById("subject").value;
-    const mensaje = document.getElementById("message").value;
-
-    // Enviar el correo electrónico utilizando los datos del formulario
-    resend.emails.send({
-        from: `${nombre} <${email}>`,
-        to: 'carlosmorales@carlosdevportfolio.com',
-        subject: asunto,
-        html: `<p>${mensaje}</p>`
-    }).then(response => {
-        console.log("Correo enviado exitosamente:", response);
-        alert("Correo enviado exitosamente");
-    }).catch(error => {
-        console.error("Error al enviar el correo:", error);
-        alert("Error al enviar el correo");
-    });
-});
+document.getElementById('msgBtn').addEventListener('click', function() {
+    // Get form values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+  
+    // Encode the message
+    const encodedMessage = encodeURIComponent(
+      `Nombre: ${name}\nEmail: ${email}\nAsunto: ${subject}\nMensaje: ${message}`
+    );
+  
+    // WhatsApp URL
+    const whatsappURL = `https://wa.me/523333720692?text=${encodedMessage}`;
+  
+    // Redirect to WhatsApp
+    window.location.href = whatsappURL;
+  });
